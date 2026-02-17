00f9c59d93187b3584b927e342612b1aca4cbeb6
Adaptive Shield Matrix <36697098+adaptive-shield-matrix@users.noreply.github.com>
2026-02-17 11:07:19 +0600

    feat(caching): cache served static content from public for 3 days

--- src/server/hono.ts
+++ src/server/hono.ts
@@ -1,13 +1,20 @@
+import type { Env } from "@/env/Env"
+import { addRoutesOpenapi } from "@/server/routes/addRoutesOpenapi"
+import { addRoutesServer } from "@/server/routes/addRoutesServer"
 import { Hono } from "hono"
 import { serveStatic } from "hono/bun"
-import type { Env } from "@/env/Env"
-import { addRoutesServer } from "@/server/routes/addRoutesServer"
-import { addRoutesOpenapi } from "@/server/routes/addRoutesOpenapi"
 
 export function createApp(): Hono<{ Bindings: Env }> {
   const app = new Hono<{ Bindings: Env }>()
 
-  app.use("/*", serveStatic({ root: "./public" }))
+  app.use("/*", async (c, next) => {
+    const res = await serveStatic({ root: "./public" })(c, next)
+    if (res) {
+      // (3 days = 259200 seconds)
+      res.headers.set("Cache-Control", "public, max-age=259200, immutable")
+    }
+    return res
+  })
 
   addRoutesServer(app)
   addRoutesOpenapi(app)
